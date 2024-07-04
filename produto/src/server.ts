e66b5abf38bc680444137ac8a7d9f7ce748e8334
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-07-04 09:37:44 -0300

    feat: add route endpoint product

--- produto/src/server.ts
+++ produto/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { router } from "./infra/routes";
 
 const PORT = process.env.PORT ?? 3333;
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () =>
   console.log("HTTP server product running on port " + PORT)
